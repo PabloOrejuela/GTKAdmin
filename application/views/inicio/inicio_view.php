e507abcba419f4bcee18a8d5774f849e42386104
Pablo Orejuela <pabloorejuela@hotmail.com>
2022-04-20 18:25:50 -0500

    Incluída la imagen del producto en la vista de inicio

--- application/views/inicio/inicio_view.php
+++ application/views/inicio/inicio_view.php
@@ -1,6 +1,14 @@
-<div id="wrap">
+<style>
+	#te-fondo {
+		opacity: 0.3;
+	}
+</style>
+<div id="wrap" style="margin-top: 60px;">
+	<div style="float: left;margin-right: 30px;">
+		<img src="<?php echo base_url().'images/'.'te'; ?>.png" alt="zafiro" width="120" id="te-fondo" />
+	</div>
     <div style="margin-top: 60px;">
         <h4>Bienvenidos al <br>Sitio de Administración de GTK Ecuador</h4>                
     </div>
-	<img src="<?php echo base_url().'images/niveles/'.'person'; ?>.png" alt="zafiro" width="300">
+	
 </div>
